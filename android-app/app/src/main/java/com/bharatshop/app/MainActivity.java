b980974ae4fcf64b57ff8593e6b1cbd16fccc3f2
mullafaiz666-spec <mullafaiz666@gmail.com>
2026-09-09 14:25:27 +0530

    Ship Android 1.3 with forced fresh production storefront

--- android-app/app/src/main/java/com/bharatshop/app/MainActivity.java
+++ android-app/app/src/main/java/com/bharatshop/app/MainActivity.java
@@ -9,7 +9,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-    private static final String START_URL = "https://bharatshop-9w4a.onrender.com";
+    private static final String START_URL = "https://bharatshop-9w4a.onrender.com/?app=android-1.3";
     private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -27,12 +27,13 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(false);
         settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
         settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setUserAgentString(settings.getUserAgentString() + " BharatShopAndroid/1.2");
+        settings.setUserAgentString(settings.getUserAgentString() + " BharatShopAndroid/1.3");
 
         CookieManager.getInstance().setAcceptCookie(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
 
         webView.clearCache(true);
+        webView.clearHistory();
         webView.setWebViewClient(new WebViewClient());
         if (savedInstanceState == null) {
             webView.loadUrl(START_URL);
